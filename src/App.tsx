7ad7a09697eab876032ad4255c1998288dd7e68f
Anna Wyroba <Anna.Wyroba@Annas-MBP.Dlink>
2024-01-31 17:32:27 +0100

    finish com task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const Square = () => {
+  return (
+    <div style={{ width: '100px', height: '100px', background: 'black'}}></div>
+  )
+};
+
 function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [numberOfSquares, setNumberOfSquares] = useState(0);
+
+  const handleOnStartClick = () => {
+    setIsRunning(true);
+  }
+
+  const handleOnStopClick = () => {
+    setIsRunning(false);
+  }
+
+  const handleOnResetClick = () => {
+    setIsRunning(false);
+    setNumberOfSquares(0);
+  }
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setNumberOfSquares((prevState) => prevState + 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [isRunning]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="example">
+        <div className="squares-box">
+          {Array.from({ length: numberOfSquares}). map((_, index) => (
+            <Square key={index} />
+          ))}
+        </div>
+        <div className="button-box">
+          <button className="button button-start" onClick={handleOnStartClick}>Start</button>
+          <button className="button button-stop" onClick={handleOnStopClick}>Stop</button>
+          <button className="button button-reset" onClick={handleOnResetClick}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
